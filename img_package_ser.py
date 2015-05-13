1f9b22441dc3b5300122144324261b77b9e357a9
SmellyCat <smellycat2014@foxmail.com>
2015-05-13 17:22:26 +0800

    V3.5.0
    1.创建package_work.py处理相关数据库操作

--- img_package_ser.py
+++ img_package_ser.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import json
-import time
-import threading
-import Queue
 import logging
 import logging.handlers
 
@@ -16,10 +12,9 @@ from flask_restful import Api
 from flask_restful import Resource
 
 import gl
+from iniconf import MyIni
 from imgdownload import Download
-from iniconf import ImgDownloadIni
-from sqlitedb import USqlite
-from cleaner import Cleaner
+from package_work import PackageWorker
 
 
 def init_logging(log_file_name):
@@ -32,7 +27,7 @@ def init_logging(log_file_name):
     logger = logging.getLogger('root')
 
     rthandler = logging.handlers.RotatingFileHandler(
-        log_file_name, maxBytes=100 * 1024 * 1024, backupCount=5)
+        log_file_name, maxBytes=20 * 1024 * 1024, backupCount=5)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s %(filename)s[line:%(lineno)d] \
@@ -43,7 +38,7 @@ def init_logging(log_file_name):
 
 def version():
     """版本号"""
-    return 'SX-UrlImgPackage V3.4.0'
+    return 'SX-UrlImgPackage V3.5.0'
 
 
 app = Flask(__name__)
@@ -73,14 +68,16 @@ class TodoList(Resource):
 
         return {'package': zipfile, 'msg': 'Success', 'code': 100}, 201
 
+
 class PackageListAPIV1(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
+
         parser.add_argument('key', type=unicode, required=True,
-                            help='Need a key value', location='json')
+                            help='A key value is require', location='json')
         parser.add_argument('urls', type=list,required=True,
-                            help='Need a url json array', location='json')
+                            help='urls json array is require', location='json')
         args = parser.parse_args()
 
         if request.json.get('key', None) not in gl.KEYSDICT:
@@ -97,75 +94,19 @@ api.add_resource(Index, '/')
 api.add_resource(TodoList, '/package')
 api.add_resource(PackageListAPIV1, '/v1/package')
 
-class PackageServer:
-
-    def __init__(self):
-        # 配置文件对象
-        self.ini = ImgDownloadIni()
-        self.sysini = self.ini.get_sys_conf()
-        # 基础路径 str
-        gl.BASEPATH = self.sysini['path'].replace("/", "\\")
-        # URL地址压缩队列 object
-        gl.MYQ = Queue.Queue()
-
-        self.port = self.sysini.get('port', 8017)
-
-        logger.warning('Sys Start')
-
-    def __del__(self):
-        # 系统退出设为真
-        gl.IS_SYS_QUIT = True
-        logger.warning('Sys Quit')
-        del self.ini
-
-    def sqlite_customer(self):
-        """sqlite数据库操作执行线程"""
-        # 创建sqlite对象
-        sqlite = USqlite()
-        # 创建数据库表
-        sqlite.create_table()
-        # 清除文件类对象
-        cl = Cleaner(sqlite)
-        for i in sqlite.get_users():
-            gl.KEYSDICT[i['key']] = {'priority': i['priority'],
-                                     'power': i['power']}
-
-        # 清理计数
-        cl_count = 0
-        while 1:
-            # 退出检测
-            if gl.IS_SYS_QUIT:
-                break
-            try:
-                sq = gl.MYQ.get(timeout=1)
-                data = json.loads(sq)
-                sqlite.add_imgdownload(
-                    data['timestamp'], data['ip'], data['path'])
-
-                if cl_count > 30:
-                    cl.clean_ot_img()
-                    cl_count = 0
-            except Queue.Empty:
-                pass
-            except Exception, e:
-                logger.error(e)
-                time.sleep(1)
-            finally:
-                cl_count += 1
-
-        del sqlite
-        del cl
-
-    def main(self):
-        t = threading.Thread(target=self.sqlite_customer, args=())
-        t.start()
-
-        app.run(host="0.0.0.0", port=self.port, threaded=True)
 
 if __name__ == '__main__':  # pragma nocover
-    init_logging(r'log\imgdownload.log')
+    init_logging(r'log\package.log')
     logger = logging.getLogger('root')
-
-    ps = PackageServer()
+    
+    ini = MyIni()
+    sysini = ini.get_sys_conf()
+    ps = PackageWorker()
     ps.main()
+    app.run(host="0.0.0.0", port=sysini.get('port', 8017), threaded=True)
+    gl.IS_SYS_QUIT = True
+    
+    del ini
     del ps
+
+
