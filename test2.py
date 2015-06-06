3979556434c50a3423a851e6bcb68836c5d5e15d
SmellyCat <smellycat2014@foxmail.com>
2015-06-07 07:07:59 +0800

    Update run

--- test2.py
+++ test2.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':  # pragma nocover
   urlstr = 'http://127.0.0.1:8017/v1/package'
   #json_data = json.dumps(['http://localhost/imgareaselect/imgs/1.jpg','http://localhost/imgareaselect/imgs/2.jpg'])
   data={'key':'sx2767722','urls': ['http://localhost/imgareaselect/imgs/1.jpg','http://localhost/imgareaselect/imgs/2.jpg']}
-  
+  print json.dumps(data)
   r = send_post(urlstr, json.dumps(data))
   print r.status_code
   print r.text
