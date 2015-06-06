85fc4395cab197f18a94a9132dc32b62a884d3c4
SmellyCat <smellycat2014@foxmail.com>
2015-06-07 06:57:29 +0800

    Add models Users

--- img_package/__init__.py
+++ img_package/__init__.py
@@ -2,7 +2,7 @@ from iniconf import MyIni
 from my_logger import debug_logging, online_logging
 from clean_worker import CleanWorker
 from app import app
-from models import User, Package
+from models import User, Users, Package
 import views
 
 __version__ = '3.6.0'
