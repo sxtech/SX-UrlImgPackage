562be5632c004bf54f7b13f7bb40fc4e4ba089f9
SmellyCat <smellycat2014@foxmail.com>
2016-03-17 22:21:46 +0800

    更新 views after_request 在init中定义

--- img_package/views.py
+++ img_package/views.py
@@ -1,66 +1,248 @@
 # -*- coding: utf-8 -*-
+import json
+from functools import wraps
 
-from flask import g, request
+import arrow
+
+from flask import g, request, abort
 from flask_restful import reqparse, abort, Resource
 from passlib.hash import sha256_crypt
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from app import app, db, api, auth, logger
-from models import User, Users
-from imgdownload import Download
-
-
-@app.before_request
-def before_request():
-    g.db = db
-    g.db.connect()
+from . import app, db, api, auth, logger, access_logger
+from models import Users, Scope, Package
+from help_func import *
+from downloader import Downloader
 
 
-@app.after_request
-def after_request(response):
-    g.db.close()
-    return response
-
+def verify_addr(f):
+    """IP地址白名单"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not app.config['WHITE_LIST_OPEN'] or request.remote_addr in app.config['WHITE_LIST']:
+            pass
+        else:
+            return {'status': '403.6',
+                    'error': u'禁止访问:客户端的 IP 地址被拒绝'}, 403
+        return f(*args, **kwargs)
+    return decorated_function
 
 @auth.verify_password
 def verify_password(username, password):
-    user = Users.get_one(Users.username == username,
-                         Users.banned == False)
-    if not user:
+    if username.lower() == 'admin':
+        user = Users.query.filter_by(username='admin').first()
+    else:
         return False
-    return sha256_crypt.verify(password, user.password)
+    if user:
+        return sha256_crypt.verify(password, user.password)
+    return False
+
+
+def verify_token(f):
+    """token验证装饰器"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not request.headers.get('Access-Token'):
+            return {'status': '401.6', 'message': 'missing token header'}, 401
+        token_result = verify_auth_token(request.headers['Access-Token'],
+                                         app.config['SECRET_KEY'])
+        if not token_result:
+            return {'status': '401.7', 'message': 'invalid token'}, 401
+        elif token_result == 'expired':
+            return {'status': '401.8', 'message': 'token expired'}, 401
+        g.uid = token_result['uid']
+        g.scope = set(token_result['scope'])
+
+        return f(*args, **kwargs)
+    return decorated_function
+
+def verify_scope(f):
+    """权限范围验证装饰器"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        try:
+            scope = '_'.join([request.path[1:], request.method.lower()])
+            print scope
+        except Exception as e:
+            print (e)
+        if 'all' in g.scope or scope in g.scope:
+            pass
+        else:
+            return {'status': 405, 'error': 'Method Not Allowed'}, 405
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 class Index(Resource):
 
     def get(self):
-        return {'package_url': 'http://localhost/package',
-                'package_v1_url': 'http://localhost/v1/package'}
+        return {
+            'user_url': 'http://%s:%s/user{/user_id}' % (request.remote_addr, app.config['PORT']),
+            'scope_url': 'http://%s:%s/scope' % (request.remote_addr, app.config['PORT']),
+            'token_url': 'http://%s:%s/token' % (request.remote_addr, app.config['PORT']),
+            'package_url': 'http://%s:%s/package' % (request.remote_addr, app.config['PORT'])
+        }, 200, {'Cache-Control': 'public, max-age=60, s-maxage=60'}
 
 
-class TodoList(Resource):
+class User(Resource):
+    #decorators = [verify_token, limiter.limit("50/minute")]
+
+    @verify_addr
+    @verify_scope
+    def get(self, user_id):
+        user = Users.query.filter_by(id=user_id, banned=0).first()
+        if user:
+            return {'id': user.id,
+                    'username': user.username,
+                    'scope': user.scope,
+                    'date_created': str(user.date_created),
+                    'date_modified': str(user.date_modified),
+                    'banned': user.banned}, 200
+        else:
+            return {}, 404
+
+    @verify_addr
+    @verify_scope
+    def put(self, user_id):
+        verify_scope('user_put')
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('scope', type=unicode, required=True,
+                            help='A scope field is require', location='json')
+        args = parser.parse_args()
+
+        # 所有权限范围
+        all_scope = set()
+        for i in Scope.query.all():
+            all_scope.add(i.name)
+        # 授予的权限范围
+        request_scope = set(request.json.get('scope', u'null').split(','))
+        # 求交集后的权限
+        u_scope = ','.join(all_scope & request_scope)
+
+        db.session.query(Users).filter_by(id=user_id).update({'scope': u_scope, 'date_modified': arrow.now().datetime})
+        db.session.commit()
+
+        user = Users.query.filter_by(id=user_id).first()
+        app.config['SCOPE_USER'][user.id] = set(user.scope.split(','))
+
+        return {
+            'id': user.id,
+            'username': user.username,
+            'scope': user.scope,
+            'date_created': str(user.date_created),
+            'date_modified': str(user.date_modified),
+            'banned': user.banned
+        }, 201
+
+
+class UserList(Resource):
+    #decorators = [verify_token, limiter.limit("50/minute")]
+
+    @verify_addr
+    @verify_scope
+    def post(self):
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+
+        user = Users.query.filter_by(username=request.json['username'],
+                                     banned=0).first()
+        if not user:
+            password_hash = sha256_crypt.encrypt(request.json['password'],
+                                                 rounds=app.config['ROUNDS'])
+            # 所有权限范围
+            all_scope = set()
+            for i in Scope.query.all():
+                all_scope.add(i.name)
+            # 授予的权限范围
+            request_scope = set(request.json.get('scope', u'null').split(','))
+            # 求交集后的权限
+            u_scope = ','.join(all_scope & request_scope)
+            u = Users(username=request.json['username'],
+                      password=password_hash, scope=u_scope, banned=0)
+            db.session.add(u)
+            db.session.commit()
+            return {
+                'id': u.id,
+                'username': u.username,
+                'scope': u.scope,
+                'date_created': str(u.date_created),
+                'date_modified': str(u.date_modified),
+                'banned': u.banned
+            }, 201
+        else:
+            return {'message': 'username is already esist'}, 422
+
+
+class ScopeList(Resource):
+
+    @verify_addr
+    @verify_token
+    @verify_scope
+    def get(self):
+        scope = Scope.query.all()
+        items = []
+        for i in scope:
+            items.append(row2dict(i))
+        return {'total_count': len(items), 'items': items}, 200
+
+
+def get_uid():
+    try:
+        user = Users.query.filter_by(username=request.json.get('username', ''),
+                                     banned=0).first()
+        g.uid = -1
+    except Exception as e:
+        logger.error(e)
+        raise
+    if user:
+        if sha256_crypt.verify(request.json.get('password', ''), user.password):
+            g.uid = user.id
+            g.scope = user.scope
+    return str(g.uid)
+
+
+class TokenList(Resource):
+    #decorators = [limiter.limit("5/hour", get_uid), verify_addr]
 
     def post(self):
-        if not request.json:
-            return {'package': None, 'msg': 'Bad Request', 'code': 400}, 400
-        if User.get_one(User.username == request.json.get('key', None)) is None:
-            return {'package': None, 'msg': 'Key Error', 'code': 105}, 401
-        if 'urls' not in request.json:
-            return {'package': None, 'msg': 'No "urls" key', 'code': 106}, 400
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if g.uid == -1:
+            return {'message': 'username or password error'}, 422
+        s = Serializer(app.config['SECRET_KEY'],
+                       expires_in=app.config['EXPIRES'])
+        token = s.dumps({'uid': g.uid, 'scope': g.scope.split(',')})
+        return {
+            'uid': g.uid,
+            'access_token': token,
+            'token_type': 'self',
+            'scope': g.scope,
+            'expires_in': app.config['EXPIRES']
+        }, 201, {'Cache-Control': 'no-store', 'Pragma': 'no-cache'}
 
-        imgd = Download(request.remote_addr)
-        zipfile = imgd.main(request.json.get('urls', []))
-        del imgd
 
-        return {'package': zipfile, 'msg': 'Success', 'code': 100}, 201
+class PackageList(Resource):
+    #decorators = [limiter.limit("60/minute")]
 
-
-class PackageListAPIV1(Resource):
-
-    @auth.login_required
+    #@verify_addr
+    #@verify_token
     def post(self):
         if not request.json:
             return {'message': 'Problems parsing JSON'}, 400
-        if 'urls' not in request.json.keys():
+        if request.json.get('urls', None) is None:
             return {'message': 'Validation Failed',
                     'errors': [{'resource:': 'Package', 'field': 'urls',
                                 'code': 'missing_field'}]}, 422
@@ -68,13 +250,23 @@ class PackageListAPIV1(Resource):
             return {'message': 'Validation Failed',
                     'errors': [{'resource:': 'Package', 'field': 'urls',
                                 'code': 'invalid'}]}, 422
+        try:
+            d = Downloader()
+            d_dict = d.main(request.json['urls'])
+            package = Package(
+                ip=request.remote_addr, path=d_dict['zipname'],
+                date_created=arrow.now().datetime)
+            db.session.add(package)
+            db.session.commit()
+        except Exception as e:
+            logger.error(e)
+        return {'package': d_dict['name']}, 201
 
-        imgd = Download(request.remote_addr)
-        zipfile = imgd.main(request.json['urls'])
-        del imgd
-
-        return {'package': zipfile}, 201
 
 api.add_resource(Index, '/')
-api.add_resource(TodoList, '/package')
-api.add_resource(PackageListAPIV1, '/v1/package')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserList, '/user')
+api.add_resource(ScopeList, '/scope')
+api.add_resource(TokenList, '/token')
+api.add_resource(PackageList, '/package')
+
